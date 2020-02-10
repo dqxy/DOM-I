f5051c27605bfa4a9bd847a527ac901fd46b0f5a
dqxy <yanofsky.david@gmail.com>
2020-02-10 17:18:14 -0500

    JS

--- js/index.js
+++ js/index.js
@@ -28,7 +28,8 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address1" : "123 Way 456 Street",
+    "address2" : "Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -40,3 +41,69 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"])
+
+let mis = document.getElementById("middle-img");
+mis.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const nav = document.querySelectorAll('a');
+nav[0].textContent = 'Services';
+nav[1].textContent = 'Product';
+nav[2].textContent = 'Vision';
+nav[3].textContent = 'Features';
+nav[4].textContent = 'About';
+nav[5].textContent = 'Contact';
+
+
+const h1 = document.querySelectorAll('h1');
+h1[0].textContent = 'DOM';
+var br = document.createElement("br");
+h1[0].appendChild(br);
+h1[0].innerHTML  += 'Is';
+h1[0].appendChild(br);
+h1[0].innerHTML += 'Awesome';
+
+const button = document.querySelectorAll('button');
+button[0].textContent = 'Get Started';
+
+const h4 = document.querySelectorAll('h4');
+h4[0].textContent = siteContent["main-content"]["features-h4"];
+h4[1].textContent = siteContent["main-content"]["about-h4"];
+h4[2].textContent = siteContent["main-content"]["services-h4"];
+h4[3].textContent = siteContent["main-content"]["product-h4"];
+h4[4].textContent = siteContent["main-content"]["vision-h4"];
+h4[5].textContent = siteContent["contact"]["contact-h4"];
+
+const p = document.querySelectorAll('p');
+p[0].textContent = siteContent["main-content"]["features-content"];
+p[1].textContent = siteContent["main-content"]["about-content"];
+p[2].textContent = siteContent["main-content"]["services-content"];
+p[3].textContent = siteContent["main-content"]["product-content"];
+p[4].textContent = siteContent["main-content"]["vision-content"];
+p[5].textContent = siteContent["contact"]["address1"];
+p[5].appendChild(br);
+p[5].innerHTML += siteContent["contact"]["address2"];
+p[6].textContent = siteContent["contact"]["phone"];
+p[7].textContent = siteContent["contact"]["email"];
+p[8].textContent = siteContent["footer"]["copyright"];
+
+nav[0].style.color = "green";
+nav[1].style.color = "green";
+nav[2].style.color = "green";
+nav[3].style.color = "green";
+nav[4].style.color = "green";
+nav[5].style.color = "green";
+
+var newElem1 = document.createElement("a");
+newElem1.innerHTML = "Food";
+newElem1.style.color = "green";
+
+var newElem2 = document.createElement("a");
+newElem2.innerHTML = "Chairs";
+newElem2.style.color = "green";
+
+var navtag = document.getElementsByTagName("nav");
+navtag[0].appendChild(newElem1);
+navtag[0].prepend(newElem2);
